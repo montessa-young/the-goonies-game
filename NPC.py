5321cb1c90173307ea0459417beea464318ea32a
bigboijesus3 <jarizmendi263@gmail.com>
2026-03-30 13:48:24 +0000

    forgot what i added

--- NPC.py
+++ NPC.py
@@ -1,4 +1,7 @@
 import BlacksmithBuyables.py
+from BlacksmithBuyables.py import Headgear_Buyables
+import time
+import random
 #----------------------
 # Davey (Black Smith)
 #----------------------
@@ -6,6 +9,7 @@ import BlacksmithBuyables.py
 class NPC:
     def Davey():
         Intro_Davey_Dialogue = ("Davey: Would you like to buy; Headgear, Facegear, Undershirtgear, Overshirtgear, Belt, Pants, Shoes?")
+        print(Intro_Davey_Dialogue)
         Choice_001 = input()
         if Choice_001 == "Headgear" or "headgear":
             print(Headgear_Buyables)
@@ -15,7 +19,28 @@ class NPC:
 # Micheal Jackson (Intro)
 #----------------------
 
-
+class NPC:
+    def MikeJack():
+        while True:
+            autosaveran = random.randint(1,6)
+            Intro_MikeJack_Dial = ("Michael Jackson: Would you like to go on a quick adventure with me? y/n")
+            yn_choice = ()
+            if yn_choice == "y":
+                print("Awesome sauce! Come with me to your first battle!")
+                break
+            elif yn_choice == "n":
+                print("Too bad! You can't play the game without starting it!")
+                break
+            else:
+                print("Please retry, invalid option")
+        time.sleep(1)
+        print("Hey, just to let you know that 8 is the pause menu, and 9 is the inventory button,")
+        print("the menu or inventory will NOT be available during quick times, boss battles, shops,")
+        print("etc., etc., so be prepared for that")
+        time.sleep(10)
+        print("Auto Saving...")
+        time.sleep(autosaveran)
+        Davey()
 
 #----------------------
 # Mario (50 Mafia Boss)
